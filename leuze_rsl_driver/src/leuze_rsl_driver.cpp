5e6b6dc2767dda276604372632b513a014e52a39
kimsooyoung <tge1375@naver.com>
2023-05-17 21:09:25 +0900

    bf refactoring

--- leuze_rsl_driver/src/leuze_rsl_driver.cpp
+++ leuze_rsl_driver/src/leuze_rsl_driver.cpp
@@ -2,27 +2,10 @@
 // #include "leuze_rsl_driver/rsl400_interface.hpp"
 #include "leuze_rsl_driver/my_node.hpp"
 
-// int main(int argc, char *argv[])
-// {
-//     ros::init(argc, argv, "leuze_driver");
-//     ros::NodeHandle n;
-//     if (argc < 3)
-//     {
-//         std::cerr << "Not enough arguments!" << std::endl;
-//     }
-//     std::string address = argv[1];
-//     std::string port = argv[2];
-
-//     RSL400Interface rsl_interface(address, port, &n);
-//     rsl_interface.connect();
-
-//     ros::spin();
-//     return 0;
-// }
-
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
 
+  // TODO: into node param
   std::string address("192.168.10.1");
   std::string port("9990");
 
